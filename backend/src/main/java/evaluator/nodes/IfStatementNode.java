affbd5f392261619b2f9018f2f4fc0bdd1ec4a16
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-15 16:46:27 +0700

    implement IfStatementNode.java

--- backend/src/main/java/evaluator/nodes/IfStatementNode.java
+++ backend/src/main/java/evaluator/nodes/IfStatementNode.java
@@ -1,8 +1,21 @@
 package evaluator.nodes;
 
 public class IfStatementNode implements Node {
+    protected Node ExprNode;
+    protected Node trueStatement, falseStatement;
+
+    public IfStatementNode(Node ExprNode, Node trueStatement, Node falseStatement){
+        this.ExprNode = ExprNode;
+        this.trueStatement = trueStatement;
+        this.falseStatement = falseStatement;
+    }
     @Override
     public long evaluate() {
+        if (ExprNode.evaluate() == 1){
+            trueStatement.evaluate();
+        }else{
+            falseStatement.evaluate();
+        }
         return 0;
     }
 }
