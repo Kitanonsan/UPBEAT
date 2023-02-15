6db72cc3c9f3ae9afcaafbdeb51bb7898abe9ce2
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-15 22:47:20 +0700

    implement AssignmentStatementNode.java

--- backend/src/main/java/evaluator/nodes/AssignmentStatementNode.java
+++ backend/src/main/java/evaluator/nodes/AssignmentStatementNode.java
@@ -1,8 +1,16 @@
 package evaluator.nodes;
 
 public class AssignmentStatementNode implements Node {
+    protected IdentifierNode variable;
+    protected Node ExprNode;
+
+    public AssignmentStatementNode(IdentifierNode variable, Node ExprNode){
+        this.variable = variable;
+        this.ExprNode = ExprNode;
+    }
     @Override
     public long evaluate() {
+        variable.assign(ExprNode.evaluate());
         return 0;
     }
 }
