c4c0c210ffaa3b172ba9476df812c44ec345901e
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-15 22:41:20 +0700

    implement IdentifierNode.java

--- backend/src/main/java/evaluator/nodes/IdentifierNode.java
+++ backend/src/main/java/evaluator/nodes/IdentifierNode.java
@@ -1,8 +1,23 @@
 package evaluator.nodes;
 
+import java.util.Map;
+
 public class IdentifierNode implements Node{
+    protected Map<String, Long> variables;
+    protected String identifier;
+    public IdentifierNode(Map<String, Long> variables, String identifier){
+        this.variables = variables;
+        this.identifier = identifier;
+    }
     @Override
     public long evaluate() {
-        return 0;
+        if (!variables.containsKey(identifier)){
+            variables.put(identifier,0L);
+        }
+        return variables.get(identifier);
+    }
+
+    public void assign(Long value){
+        variables.put(identifier, value);
     }
 }
