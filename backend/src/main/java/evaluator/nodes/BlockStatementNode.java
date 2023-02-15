75473159f8a9c22c48fbbdf2e62d066687a87dd8
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-15 22:46:45 +0700

    implement BlockStatementNode.java

--- backend/src/main/java/evaluator/nodes/BlockStatementNode.java
+++ backend/src/main/java/evaluator/nodes/BlockStatementNode.java
@@ -1,6 +1,17 @@
 package evaluator.nodes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockStatementNode implements Node {
+    protected List<Node> statements;
+    public BlockStatementNode(){
+        statements = new ArrayList<>();
+    }
+
+    public void add(Node statementNode){
+        statements.add(statementNode);
+    }
     @Override
     public long evaluate() {
         return 0;
