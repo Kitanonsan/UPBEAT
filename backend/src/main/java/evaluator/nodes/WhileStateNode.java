9d5070b97ac8033e2ba71709fdb1c9f7e7791d47
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-15 16:54:03 +0700

    implement WhileStatementNode.java

--- backend/src/main/java/evaluator/nodes/WhileStateNode.java
+++
@@ -1,8 +0,0 @@
-package evaluator.nodes;
-
-public class WhileStateNode implements Node {
-    @Override
-    public long evaluate() {
-        return 0;
-    }
-}
--- backend/src/main/java/evaluator/nodes/WhileStatementNode.java
+++ backend/src/main/java/evaluator/nodes/WhileStatementNode.java
@@ -1,9 +1,17 @@
 package evaluator.nodes;
 
 public class WhileStatementNode implements Node {
-
+    protected Node ExprNode;
+    protected Node StatementNode;
+    public WhileStatementNode(Node ExprNode, Node StatementNode){
+        this.ExprNode = ExprNode;
+        this.StatementNode = StatementNode;
+    }
     @Override
     public long evaluate() {
+        for (int counter = 0; counter < 10000 && ExprNode.evaluate() > 0; counter++){
+            StatementNode.evaluate();
+        }
         return 0;
     }
 }
