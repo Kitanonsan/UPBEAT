7e7f7365a3ddd28d45af40387cd2bc3948aef318
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-16 12:55:43 +0700

    implement NotImplementYet exception

--- backend/src/main/java/evaluator/nodes/RegionCommandNode.java
+++ backend/src/main/java/evaluator/nodes/RegionCommandNode.java
@@ -1,8 +1,9 @@
 package evaluator.nodes;
 
-public class RegionCommandNode implements Node{
+public class RegionCommandNode implements Node{ // relocate, invest, collect
     @Override
-    public long evaluate() {
-        return 0;
+    public long evaluate() { //todo implement regioncommand
+        throw new NotImplementYet();
+//        return 0;
     }
 }
