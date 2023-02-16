9c56fe27fc88dc18b257042dab2cc3d8f1e9b71a
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-16 12:56:11 +0700

    implement NotImplementYet exception

--- backend/src/main/java/evaluator/nodes/MoveCommandNode.java
+++ backend/src/main/java/evaluator/nodes/MoveCommandNode.java
@@ -1,8 +1,12 @@
 package evaluator.nodes;
 
+
 public class MoveCommandNode implements Node {
+    //ไม่รู้ว่า move แล้วจะไปไหน ต้องมีส่วนที่บอกว่า move แล้วจะขยับยังไง
+    protected String direction;
     @Override
-    public long evaluate() {
-        return 0;
+    public long evaluate() { //Todo implement evalutae
+        throw new NotImplementYet();
+//        return 0;
     }
 }
