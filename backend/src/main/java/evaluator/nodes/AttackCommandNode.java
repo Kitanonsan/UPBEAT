fed7759da425a1d3aaa292c208435eb33e5bfbeb
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-16 12:55:59 +0700

    implement NotImplementYet exception

--- backend/src/main/java/evaluator/nodes/AttackCommandNode.java
+++ backend/src/main/java/evaluator/nodes/AttackCommandNode.java
@@ -1,8 +1,15 @@
 package evaluator.nodes;
 
-public class AttackCommandNode implements Node{
+public class AttackCommandNode implements Node{ //shoot
+    protected String direction;
+    protected Node ExprNode;
+    public AttackCommandNode(String direction, Node ExprNode){
+        this.direction = direction;
+        this.ExprNode = ExprNode;
+    }
     @Override
-    public long evaluate() {
-        return 0;
+    public long evaluate() { //Todo implement evaluate Shoot command
+        throw new NotImplementYet();
+//        return 0;
     }
 }
