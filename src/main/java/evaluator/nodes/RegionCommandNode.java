2f0729bf9e1c16a1604c5d44afda1b2b75fa3e5f
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-17 22:14:44 +0700

    implement RegionCommandNode

--- src/main/java/evaluator/nodes/RegionCommandNode.java
+++ src/main/java/evaluator/nodes/RegionCommandNode.java
@@ -1,6 +1,15 @@
 package evaluator.nodes;
 
-public class RegionCommandNode implements Node{ // relocate, invest, collect
+import game.Player;
+
+public class RegionCommandNode implements Node{ // invest, collect
+    protected String financeMode;
+    protected Player player;
+
+    public RegionCommandNode(String financeMode, Player player){
+        this.financeMode = financeMode;
+        this.player = player;
+    }
     @Override
     public long evaluate() { //todo implement regioncommand
         throw new NotImplementYet();
