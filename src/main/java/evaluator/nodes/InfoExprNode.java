fec722610cd4752ce66dc621c72bd01ef09ded63
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-17 23:41:00 +0700

    create and implement InfoExprNode

--- src/main/java/evaluator/nodes/InfoExprNode.java
+++ src/main/java/evaluator/nodes/InfoExprNode.java
@@ -1,6 +1,15 @@
 package evaluator.nodes;
 
-public class InfoExprNode implements Node{
+import game.Player;
+
+public class InfoExprNode implements Node{ // opponent | nearby
+    protected String info;
+    protected Player player;
+
+    public InfoExprNode(String info, Player player){
+        this.info = info;
+        this.player = player;
+    }
     @Override
     public long evaluate() {
         return 0;
