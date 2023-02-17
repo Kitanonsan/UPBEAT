e3c70545a0a7b96c3a1594c6f76ec07ebfededc5
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-17 22:13:09 +0700

    implement DoneNode.java

--- src/main/java/evaluator/nodes/DoneNode.java
+++ src/main/java/evaluator/nodes/DoneNode.java
@@ -1,8 +1,15 @@
 package evaluator.nodes;
 
+import game.Player;
+
 public class DoneNode implements Node{
+    Player player;
+    public DoneNode(Player player){
+        this.player = player;
+    }
     @Override
     public long evaluate() {
+//        player.done();
         return 0;
     }
 }
