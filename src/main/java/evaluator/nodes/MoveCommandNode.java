7efdfc3e0d5905dc99c4b29b68879f92d1127c15
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-17 22:13:45 +0700

    implement MoveCommandNode.java

--- src/main/java/evaluator/nodes/MoveCommandNode.java
+++ src/main/java/evaluator/nodes/MoveCommandNode.java
@@ -1,9 +1,13 @@
 package evaluator.nodes;
 
+import game.Player;
+
 public class MoveCommandNode implements Node {
     protected String direction;
-    public MoveCommandNode(String direction){
+    Player player;
+    public MoveCommandNode(String direction, Player player){
         this.direction = direction;
+        this.player = player;
     }
     @Override
     public long evaluate() { //Todo implement evalutae
