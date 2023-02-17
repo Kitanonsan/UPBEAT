21e0812e74ae9cf532441b5d43f7c1690660e401
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-18 01:03:56 +0700

    add direction

--- src/main/java/evaluator/nodes/InfoExprNode.java
+++ src/main/java/evaluator/nodes/InfoExprNode.java
@@ -4,14 +4,17 @@ import game.Player;
 
 public class InfoExprNode implements Node{ // opponent | nearby
     protected String info;
+    protected String direction;
     protected Player player;
 
-    public InfoExprNode(String info, Player player){
+    public InfoExprNode(String info, String direction,Player player){
         this.info = info;
+        this.direction = direction;
         this.player = player;
     }
     @Override
     public long evaluate() {
+//        player.checkRegion(info, direction, player);
         return 0;
     }
 }
