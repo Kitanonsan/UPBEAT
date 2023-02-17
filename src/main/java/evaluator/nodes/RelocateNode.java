64ec1c2efb520910dcb44875cc77ebb278d83c76
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-17 22:15:17 +0700

    implement RelocateNode.java

--- src/main/java/evaluator/nodes/RelocateNode.java
+++ src/main/java/evaluator/nodes/RelocateNode.java
@@ -1,6 +1,15 @@
 package evaluator.nodes;
 
+import game.Player;
+
 public class RelocateNode implements Node{
+    protected String direction;
+    Player player;
+
+    public RelocateNode(String direction, Player player){
+        this.direction = direction;
+        this.player = player;
+    }
     @Override
     public long evaluate() {
         return 0;
