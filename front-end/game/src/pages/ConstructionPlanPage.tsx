3c02cee28478387f0a7f110074f8e9849bbeb46b
Pichayoot <pichayoothunchainao@gmail.com>
2023-02-21 23:20:59 +0700

    Add Read ConstructionPlan on starter ConstructionPlanPage and write to ConstructionPlan.txt

--- front-end/game/src/pages/ConstructionPlanPage.tsx
+++ front-end/game/src/pages/ConstructionPlanPage.tsx
@@ -1,18 +1,48 @@
+import { useState } from "react";
+import axios from "axios";
 import Link from "next/link";
 
 export default function Start() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleClick = async () => {
+    try {
+      await axios.post("/api/write-file", { inputValue });
+      console.log("Input saved!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className="ConstructionPlan_Container">
       <div>
         <h1 className="Text_ConstructionPLan">Construction Plan</h1>
       </div>
       <div className="ConstructionPlanHelp">{/* <img src="/U.png" /> */}</div>
-      <input className="InputConstructionPlan" />
+      <div style={{ position: "relative" }}>
+        <input
+          className="InputConstructionPlan"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button
+          className="Done_button_Con"
+          style={{ position: "absolute", bottom: 5, right: 5 }}
+          onClick={handleClick}
+        >
+          Done
+        </button>
+      </div>
       <div className="ButtonGroup">
-        <Link href="/StartGamePage">
+        <Link href="/">
           <button className="Back_button_Con">BACK</button>
         </Link>
-        <Link href="/">
+        <Link href="/Game">
           <button className="Go_button_Con">GO</button>
         </Link>
       </div>
---
+++ front-end/game/src/pages/api/write-file.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { inputValue } = req.body;
+  fs.writeFile("ConstructionPlan.txt", inputValue, function (err) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error writing file");
+    } else {
+      console.log("Input saved!");
+      res.status(200).send("File written successfully");
+    }
+  });
+}
