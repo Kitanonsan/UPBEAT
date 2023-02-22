9e7b0ef8f49b8fcab438205ac760fa1e0c4808a5
Pichayoot <pichayoothunchainao@gmail.com>
2023-02-22 15:52:58 +0700

    Add pop up Change construction plan in game page

--- front-end/game/src/components/ConstructionplanButton.tsx
+++ front-end/game/src/components/ConstructionplanButton.tsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
+
 export default function ConstructionButton() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [constructionPlan, setConstructionPlan] = useState("");
+
+  const handleButtonClick = () => {
+    setIsClicked(true);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newConstructionPlan = event.target.value;
+    setConstructionPlan(newConstructionPlan);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Do something with the constructionPlan value, e.g. submit it to a server
+    console.log("Construction plan submitted:", constructionPlan);
+    setIsClicked(false);
+    setConstructionPlan("");
+  };
+
   return (
-    <a href="">
-      <button className="Construction-button">Change Construction Plan</button>
-    </a>
+    <div className="form-wrapper">
+      {isClicked ? (
+        <form onSubmit={handleSubmit} className="form">
+          <label className="label">
+            <h3>Enter Construction Plan : </h3>
+            <input
+              type="text"
+              value={constructionPlan}
+              onChange={handleInputChange}
+              className="input"
+            />
+          </label>
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </form>
+      ) : (
+        <button className="Construction-button" onClick={handleButtonClick}>
+          Change Construction Plan
+        </button>
+      )}
+    </div>
   );
 }
