332abb345bd1d3064d4f54a69b99ec85585179f2
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-22 11:46:43 +0700

    fixed regex \b for reserve word

--- src/main/java/evaluator/Regex.java
+++ src/main/java/evaluator/Regex.java
@@ -3,7 +3,7 @@ package evaluator;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public static String Regex = "([+*/%^-])|([0-9]+)|(=)|([{()}])|(collect|done|down|downleft|downright|up|upleft|upright|invest|opponent|nearby|move|relocate|shoot|if|else|then|while)|([a-zA-Z]+[a-zA-Z0-9]*)|([^ \\r\\n]+)";
+    public static String Regex = "([+*/%^-])|([0-9]+)|(=)|([{()}])|\\d(collect|done|down|downleft|downright|up|upleft|upright|invest|opponent|nearby|move|relocate|shoot|if|else|then|while)\\d|([a-zA-Z]+[a-zA-Z0-9]*)|([^ \\r\\n]+)";
 
     public static String Operator = "[+*/%^-]";
     public static String Number = "[0-9]+";
