20e4d0862e334bae2fabc3730fe81a95264515ae
Jabbwa <sopida_kit@cmu.ac.th>
2023-02-23 19:43:27 +0700

    fixed GrammarParser มีการเรียกใช้ parse ผิดพลาด

--- src/main/java/evaluator/GrammarParser.java
+++ src/main/java/evaluator/GrammarParser.java
@@ -39,52 +39,13 @@ public class GrammarParser implements Parser{
         return v;
     }
 
-    public Node parsePlan() throws SyntaxError{
-            Node statementNote = parseStatement();
-            return statementNote;
-//        BlockStatementNode blockStatement = new BlockStatementNode();
-//        while (tkz.hasNextToken()){
-//            String v = tkz.peek();
-//            if(v.matches(Regex.Number) || v.matches(Regex.Operator) || v.matches(Regex.Direction) || v.matches(Regex.InfoExpr)){
-//                tkz.consume();
-//                throw new SyntaxError("wrong stater word");
-//            }else if(v.matches(Regex.ReservedWord)){ //reserved words
-//                if (v.matches(Regex.If)){ //todo -> opponent|nearby
-//                    blockStatement.addStatement(parseIf());
-//                }else if(v.matches(Regex.While)){
-//                    blockStatement.addStatement(parseWhile());
-//                }else if (v.matches(Regex.Done)){ // done
-//                    blockStatement.addStatement(parseDone());
-//                }else if (v.matches(Regex.Relocate)) { //relocate
-//                    blockStatement.addStatement(parseRelocate());
-//                }else if (v.matches(Regex.Move)) { //move
-//                    blockStatement.addStatement(parseMove());
-//                }else if (v.matches(Regex.Shoot)) { // shoot
-//                    blockStatement.addStatement(parseAttack());
-//                }else if (v.matches(Regex.RegionCommand)) { // invest collect
-//                    blockStatement.addStatement(parseRegion());
-//                }
-//            }else if(v.matches(Regex.Variable)){ //Assign here
-//                blockStatement.addStatement(parseAssignment());
-//            }else if (v.matches(Regex.Parenthesis)){ // blockstatement
-//                if (v.equals("(") || v.equals(")")){
-//                    throw new SyntaxError("{ <- tong ma kon");
-//                } else if (v.equals("{")) {
-//                    tkz.consume();
-//                    blockStatement.addStatement(parsePlan());
-//                    if (!tkz.equals("}")){
-//                        throw new SyntaxError("mai mee -> }");
-//                    }
-//                } else if (v.equals("}")) {
-//                    break;
-//                }
-//            }else if (v.matches("\n")){
-//                tkz.consume();
-//            }
-//            else throw new SyntaxError("Syntax error: " + v);
-//
-//        }
-//        return blockStatement;
+    private Node parsePlan() throws SyntaxError{
+        BlockStatementNode nodes = new BlockStatementNode();
+        nodes.addStatement(parseStatement());
+        while (tkz.hasNextToken()) {
+            nodes.addStatement(parseStatement());
+        }
+        return nodes;
     }
 
     private Node parseStatement() throws SyntaxError{
@@ -94,8 +55,10 @@ public class GrammarParser implements Parser{
             return  parseWhile();
         }else if (tkz.peek(Regex.Parenthesis)){
             return parseBlock();
-        }else {
+        }else if (tkz.peek(Regex.ActionCommand) || tkz.peek(Regex.Variable)){
             return parseCommand();
+        }else{
+            throw new SyntaxError("Can not parse Statement");
         }
     }
 
@@ -104,10 +67,13 @@ public class GrammarParser implements Parser{
         tkz.consume("[(]");
         Node Expr = parseExpression();
         tkz.consume("[)]");
+
         tkz.consume(Regex.Then);
-        Node trueStatement = parsePlan();
+        Node trueStatement = parseStatement();
+
+//        System.out.println(tkz.peek());
         tkz.consume(Regex.Else);
-        Node falseStatement = parsePlan();
+        Node falseStatement = parseStatement();
 
         Node ifStatementNode = new IfStatementNode(Expr, trueStatement, falseStatement);
         return ifStatementNode;
@@ -118,25 +84,29 @@ public class GrammarParser implements Parser{
         tkz.consume("[(]");
         Node Expr = parseExpression();
         tkz.consume("[)]");
-        Node statement = parseExpression();
+        Node statement = parseStatement();
 
         Node whileStatement = new WhileStatementNode(Expr, statement);
         return whileStatement;
     }
 
     private Node parseBlock() throws SyntaxError{
+        System.out.println(tkz.peek());
         BlockStatementNode blockStatement = new BlockStatementNode();
-        tkz.consume("{");
-        blockStatement.addStatement(parseStatement());
-        tkz.consume("}");
+        tkz.consume("[{]");
+        while(!tkz.peek("[}]"))
+            blockStatement.addStatement(parseStatement());
+        tkz.consume("[}]");
         return blockStatement;
     }
 
     private Node parseCommand() throws SyntaxError {
         if (tkz.peek(Regex.ActionCommand)){
             return parseActionCommand();
-        }else{
+        }else if (tkz.peek(Regex.Variable)){
             return parseAssignment();
+        }else {
+            throw new SyntaxError("Can not parse Command");
         }
     }
 
@@ -157,7 +127,7 @@ public class GrammarParser implements Parser{
     }
 
 
-    public Node parseAssignment() throws SyntaxError{
+    private Node parseAssignment() throws SyntaxError{
        IdentifierNode identifier = parseIdentifier();
        tkz.consume(Regex.Assign); // =
        Node Expr = parseExpression();
@@ -167,13 +137,13 @@ public class GrammarParser implements Parser{
     }
 
 //        ActionCommand -> done | relocate | MoveCommand | RegionCommand | AttackCommand
-    public Node parseDone() throws SyntaxError{
-        tkz.peek(Regex.Done);
+    private Node parseDone() throws SyntaxError{
+        tkz.consume(Regex.Done);
 
         Node doneCommandNode = new DoneNode(player);
         return doneCommandNode;
     }
-    public Node parseMove() throws SyntaxError{
+    private Node parseMove() throws SyntaxError{
         tkz.consume(Regex.Move);
         String direction = parseDirection();
 
@@ -181,35 +151,38 @@ public class GrammarParser implements Parser{
         return moveCommandNode;
     }
 
-    public Node parseRelocate() throws SyntaxError{
+    private Node parseRelocate() throws SyntaxError{
         String direction = parseDirection();
         tkz.peek(Regex.Relocate);
 
         Node relocateCommandNode = new RelocateNode(direction, player);
         return relocateCommandNode;
     }
-    public Node parseRegion() throws SyntaxError{ //invest, collect
+    private Node parseRegion() throws SyntaxError{ //invest, collect
         String financeMode = tkz.consume();
         if (financeMode.matches(Regex.Invest)){
-            Node RegionCommandNode = new RegionCommandNode(financeMode, player);
+            Node amount = parseExpression();
+            Node RegionCommandNode = new RegionCommandNode(financeMode, amount,player);
             return RegionCommandNode;
         }else if (financeMode.matches(Regex.Collect)){
-            Node RegionCommandNode = new RegionCommandNode(financeMode, player);
+            Node amount = parseExpression();
+            Node RegionCommandNode = new RegionCommandNode(financeMode, amount,player);
             return RegionCommandNode;
         }else {
             throw new SyntaxError("Can not parse Region command");
         }
     }
 
-    public Node parseAttack() throws SyntaxError{ //shoot
+    private Node parseAttack() throws SyntaxError{ //shoot
         tkz.consume(Regex.Shoot);
         String direction = parseDirection();
+        Node expression = parseExpression();
 
-        Node attackCommandNode = new AttackCommandNode(direction, player);
+        Node attackCommandNode = new AttackCommandNode(direction, player, expression);
         return attackCommandNode;
     }
     //    Expression -> Expression + Term | Expression - Term | Term
-    public Node parseExpression() throws SyntaxError, EvalError {
+    private Node parseExpression() throws SyntaxError, EvalError {
         Node expression = parseTerm();
         while (tkz.peek("\\+") || tkz.peek("\\-")){
             String op = tkz.consume();
@@ -224,23 +197,25 @@ public class GrammarParser implements Parser{
         return expression;
     }
     //Term -> Term * Factor | Term / Factor | Term % Factor | Factor
-    public Node parseTerm() throws SyntaxError, EvalError{
+    private Node parseTerm() throws SyntaxError, EvalError{
         Node term = parseFactor();
         while (tkz.peek("\\*") || tkz.peek("/") || tkz.peek("%")){
             String op = tkz.consume();
             if(op.equals("*")){
                 term = new BinaryArithExprNode(term, "*", parseFactor());
             }else if (op.equals("/")) {
-                if(parseFactor().evaluate() == 0){
+                Node factor = parseFactor();
+                if(factor.evaluate() == 0){
                     throw new EvalError("Cannot divide by zero");
                 }else {
-                    term = new BinaryArithExprNode(term, "/", parseFactor());
+                    term = new BinaryArithExprNode(term, "/", factor);
                 }
             }else if (op.equals("%")) {
-                if (parseFactor().evaluate() == 0){
+                Node factor = parseFactor();
+                if (factor.evaluate() == 0){
                     throw new EvalError("Cannot modulo by zero");
                 }else{
-                    term = new BinaryArithExprNode(term, "%", parseFactor());
+                    term = new BinaryArithExprNode(term, "%", factor);
                 }
             }else {
                 throw new SyntaxError("parseTerm mai dai");
@@ -249,7 +224,7 @@ public class GrammarParser implements Parser{
         return  term;
     }
 //    Factor -> Power ^ Factor | Power
-    public Node parseFactor() throws SyntaxError, EvalError{
+    private Node parseFactor() throws SyntaxError, EvalError{
         Node power = parsePower();
         if (tkz.peek("\\^")){
             tkz.consume();
@@ -259,24 +234,23 @@ public class GrammarParser implements Parser{
         return power;
     }
 //    Power -> <number> | <identifier> | ( Expression ) | InfoExpression
-    public Node parsePower() throws SyntaxError{
+    private Node parsePower() throws SyntaxError{
+        System.out.println(tkz.peek());
         if (tkz.peek((Regex.Number))){ // number
-            Node numberNode = new NumberNode(Long.parseLong(tkz.peek()));
-            tkz.consume();
-            return numberNode;
+           return new NumberNode(Integer.parseInt(tkz.consume()));
+        }else if(tkz.peek(Regex.InfoExpr)){ //info
+            Node power = parseInfoExpression();
+            return power;
         }else if (tkz.peek(Regex.Variable)){ // identifier
             IdentifierNode identifierNode = parseIdentifier();
             return identifierNode;
-        }else if(tkz.peek(Regex.InfoExpr)){ //info
-           Node power = parseInfoExpression();
-           return power;
         }else if(tkz.peek("[(]")){
-            tkz.consume();
+            tkz.consume("[(]");
             Node ExprNode = parseExpression();
             if (!tkz.peek("[)]")){
                 throw new SyntaxError("unmatch -> ) ");
             }
-            tkz.consume();
+            tkz.consume("[)]");
             return ExprNode;
         }else {
             throw  new SyntaxError("Can not parsePower");
@@ -285,11 +259,11 @@ public class GrammarParser implements Parser{
     //    InfoExpression -> opponent | nearby Direction
     private Node parseInfoExpression() throws SyntaxError{
         String info = tkz.consume();
-        String direction = parseDirection();
         if (info.matches(Regex.Opponent)){
-            Node infoNode = new InfoExprNode(info, direction, player);
+            Node infoNode = new InfoExprNode(info, null, player); // TODO: fix that
             return infoNode;
         }else if(info.matches(Regex.Nearby)){
+            String direction = parseDirection();
             Node infoNode = new InfoExprNode(info, direction, player);
             return infoNode;
         }else{
@@ -297,7 +271,7 @@ public class GrammarParser implements Parser{
         }
     }
 
-    public String parseDirection() throws SyntaxError{
+    private String parseDirection() throws SyntaxError{
         String direction = tkz.peek();
         tkz.consume(Regex.Direction); //"down|downleft|downright|up|upleft|upright");
 
