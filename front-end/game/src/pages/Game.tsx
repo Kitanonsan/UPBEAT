03e33ba4d1a6d3f2a13776dd307dc4c993c2497e
Pichayoot <pichayoothunchainao@gmail.com>
2023-02-25 00:37:27 +0700

    Test create hexagon grid in game page

--- front-end/game/src/pages/Game.tsx
+++ front-end/game/src/pages/Game.tsx
@@ -1,5 +1,7 @@
 import BaseLayout from "@/components/BaseLayout";
 import { motion } from "framer-motion";
+import HexagonGrid from "@/components/HexagonGrid";
+import HexGridDemo from "../components/HexagonGrid";
 
 export default function Home() {
   return (
