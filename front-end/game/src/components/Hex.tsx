1ef9a5cf9a8d366df068065b46e24b48796980c2
Pichayoot <pichayoothunchainao@gmail.com>
2023-02-26 23:47:27 +0700

    Test render Hexagon component

--- front-end/game/src/components/Hex.tsx
+++ front-end/game/src/components/Hex.tsx
@@ -1,7 +1,20 @@
-export default function hex() {
+import React from "react";
+import Hexagon from "react-hexagon";
+
+const imageArray = [
+  { src: "GM1.png", alt: "Picture 1" },
+  { src: "GM2.png", alt: "Picture 2" },
+  { src: "GM3.png", alt: "Picture 3" },
+  { src: "GM4.png", alt: "Picture 4" },
+  { src: "GM5.png", alt: "Picture 5" },
+];
+
+export default function Hex() {
   return (
     <div>
-      <img src="" />
+      {imageArray.map((image, index) => (
+        <img key={index} src={image.src} alt={image.alt} />
+      ))}
     </div>
   );
 }
--- front-end/game/src/pages/Game.tsx
+++ front-end/game/src/pages/Game.tsx
@@ -2,11 +2,15 @@ import BaseLayout from "@/components/BaseLayout";
 import { motion } from "framer-motion";
 import HexagonGrid from "@/components/HexagonGrid";
 import HexGridDemo from "../components/HexagonGrid";
+import Hex from "@/components/Hex";
+import Hexagon from "react-hexagon";
 
 export default function Home() {
   return (
     <BaseLayout>
       <div>Center City Page</div>
+      <Hex />
+      <Hexagon flatTop backgroundImage="GM3.png" />
     </BaseLayout>
   );
 }
