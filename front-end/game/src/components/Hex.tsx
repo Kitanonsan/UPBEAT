80cfa689638ad2043b49d72c1af4fe0b972f6158
Pichayoot <pichayoothunchainao@gmail.com>
2023-02-27 16:57:17 +0700

    Create Array of component in webpage

--- front-end/game/src/components/Hex.tsx
+++ front-end/game/src/components/Hex.tsx
@@ -1,19 +1,42 @@
-import React from "react";
-import Hexagon from "react-hexagon";
+// MyComponent.tsx
 
-const imageArray = [
-  { src: "GM1.png", alt: "Picture 1" },
-  { src: "GM2.png", alt: "Picture 2" },
-  { src: "GM3.png", alt: "Picture 3" },
-  { src: "GM4.png", alt: "Picture 4" },
-  { src: "GM5.png", alt: "Picture 5" },
-];
+import Image from "next/image";
 
-export default function Hex() {
+export default function Hexgrid() {
+  const rows = 10;
+  const columns = 16;
+
+  // Initialize the matrix with null values
+  const matrix = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    matrix[i] = new Array(columns).fill(null);
+  }
+
+  // Set the image values using a loop
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const imageName = `GM${i * columns + j + 1}.png`;
+      // const imageName = "GM1.png";
+      matrix[i][j] = (
+        <Image
+          src={`/images/${imageName}`}
+          alt=""
+          width={61.5}
+          height={53}
+          onError={() => console.log(`Error loading ${imageName}`)}
+        />
+      );
+    }
+  }
+
   return (
     <div>
-      {imageArray.map((image, index) => (
-        <img key={index} src={image.src} alt={image.alt} />
+      {matrix.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          {row.map((cell: JSX.Element | null, cellIndex: number) => (
+            <span key={cellIndex}>{cell} </span>
+          ))}
+        </div>
       ))}
     </div>
   );
--- front-end/game/src/pages/Game.tsx
+++ front-end/game/src/pages/Game.tsx
@@ -1,16 +1,11 @@
 import BaseLayout from "@/components/BaseLayout";
-import { motion } from "framer-motion";
-import HexagonGrid from "@/components/HexagonGrid";
-import HexGridDemo from "../components/HexagonGrid";
-import Hex from "@/components/Hex";
-import Hexagon from "react-hexagon";
+import Hexgrid from "@/components/Hex";
 
 export default function Home() {
   return (
     <BaseLayout>
       <div>Center City Page</div>
-      <Hex />
-      <Hexagon flatTop backgroundImage="GM3.png" />
+      <Hexgrid />
     </BaseLayout>
   );
 }
