fbb1982b734c4a37d5d7122dfa18de4326c43d2a
Kitanonsan <Kitanon_san@cmu.ac.th>
2023-03-03 21:02:10 +0700

    Add cityCenter owner test

--- src/test/java/game/ConstructorTest.java
+++ src/test/java/game/ConstructorTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 
 public class ConstructorTest {
     @Test
-    void Constructor(){
+    void ConstructorTesting(){
         Territory territory = new Territory();
         Player p = new Player("Non",territory);
         p.printPosition();
         territory.showMapInfo();
+        System.out.println(territory.region(p.city_position[0],p.city_position[1]));
+        System.out.println( territory.region(p.city_position[0],p.city_position[1]).getOwner());
     }
 }
