ec0a4f9f546f4a6c00613684c5275472340dd14c
Pichayoot <pichayoothunchainao@gmail.com>
2023-03-11 16:38:12 +0700

    Change Layout

--- front-end/game/src/pages/Game.jsx
+++ front-end/game/src/pages/Game.jsx
@@ -1,12 +1,16 @@
 import BaseLayout from "@/components/BaseLayout";
 import Hexgrid from "@/components/Hex";
+import InputPlan from "../components/Input";
 import Frame from "../components/frame";
+import Timer from "../components/Timer";
 
 export default function Home() {
   return (
     <BaseLayout>
+      <Timer />
       <div>Game Page</div>
-      <Frame>{/* <Hexgrid /> */}</Frame>
+      <Hexgrid />
+      <InputPlan />
     </BaseLayout>
   );
 }
