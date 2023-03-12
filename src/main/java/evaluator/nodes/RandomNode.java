26d6733b9a02ee94ae7dceb878b335b5acab40d6
Jabbwa <sopida_kit@cmu.ac.th>
2023-03-12 14:26:51 +0700

    create and implement : RandomNode

---
+++ src/main/java/evaluator/nodes/RandomNode.java
@@ -0,0 +1,21 @@
+package evaluator.nodes;
+
+import java.util.Map;
+import java.util.Random;
+
+public class RandomNode extends IdentifierNode{
+    private Random random = null;
+
+
+    public RandomNode(String identifier, Map<String, Long> variables) {
+        super(identifier, variables);
+
+        random = new Random();
+        identifier = "random";
+    }
+
+    @Override
+    public long evaluate(){
+        return random.nextInt()%1000;
+    }
+}
