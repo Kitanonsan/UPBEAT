e43008febade58e4eaf5bcc6a3d085eee7145718
Kitanonsan <Kitanon_san@cmu.ac.th>
2023-03-13 22:59:16 +0700

    Add More Opponent Test case

--- src/test/java/game/OpponentTest.java
+++ src/test/java/game/OpponentTest.java
@@ -1,11 +1,8 @@
 package game;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 public class OpponentTest {
-
     @Test
     void UpOpponentTest(){
         for(int i = 0 ; i < Configuration.instance().m -1 ; i++){
@@ -14,9 +11,6 @@ public class OpponentTest {
                     Territory territory = new Territory();
                     Player p1 = new Player("P1",territory,Configuration.instance().m-1-i,j,0);
                     Player p2 = new Player("P2",territory,Configuration.instance().m-1-i-k,j,0);
-                    System.out.println(Arrays.toString(p1.getPosition()));
-                    System.out.println(Arrays.toString(p2.getPosition()));
-                    System.out.println(p1.opponent());
                     assertEquals(k<=6?k:0,p1.opponent());
                 }
             }
@@ -30,13 +24,44 @@ public class OpponentTest {
                     Territory territory = new Territory();
                     Player p1 = new Player("P1",territory,i,j,0);
                     Player p2 = new Player("P2",territory,i+k,j,0);
-                    System.out.println(Arrays.toString(p1.getPosition()));
-                    System.out.println(Arrays.toString(p2.getPosition()));
-                    System.out.println(p1.opponent());
                     assertEquals(k<=6?k:0,p1.opponent());
                 }
             }
         }
     }
-
+    @Test
+    void UpLeftOpponentTest(){
+        Territory territory = new Territory();
+        Player p1 = new Player("P1",territory,4,4,0);
+        Player p2 = new Player("P2",territory,3,1,0);
+        assertEquals(3,p1.opponent());
+    }
+    @Test
+    void UpRightOpponentTest(){
+        Territory territory = new Territory();
+        Player p1 = new Player("P1",territory,6,6,0);
+        Player p2 = new Player("P2",territory,3,12,0);
+        assertEquals(6,p1.opponent());
+    }
+    @Test
+    void DownLeftOpponentTest(){
+        Territory territory = new Territory();
+        Player p1 = new Player("P1",territory,2,9,0);
+        Player p2 = new Player("P2",territory,3,6,0);
+        assertEquals(3,p1.opponent());
+    }
+    @Test
+    void DownRightOpponentTest(){
+        Territory territory = new Territory();
+        Player p1 = new Player("P1",territory,3,12,0);
+        Player p2 = new Player("P2",territory,4,14,0);
+        assertEquals(2,p1.opponent());
+    }
+    @Test
+    void outOfRange(){
+        Territory territory = new Territory();
+        Player p1 = new Player("P1",territory,8,8,0);
+        Player p2 = new Player("P2",territory,0,8,0);
+        assertEquals(0,p1.opponent());
+    }
 }
