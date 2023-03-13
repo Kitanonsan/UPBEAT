0d9c9cc6daf640b8cccd2be25088638249044d6a
Kitanonsan <Kitanon_san@cmu.ac.th>
2023-03-13 22:13:15 +0700

    Add Shoot test case

--- src/test/java/game/ShootTest.java
+++ src/test/java/game/ShootTest.java
@@ -1,6 +1,6 @@
 package game;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ShootTest {
     @Test
@@ -12,6 +12,29 @@ public class ShootTest {
         p1.shoot("downleft" ,  500);
         p1.printInfo();
         System.out.println(p1.lose());
-        territory.printInfo();
+        assertTrue(p1.lose());
+    }
+
+    @Test
+    void ShootOpponentReion(){
+        Territory territory = new Territory();
+        Player p1 = new Player("P1",territory,5,5,10000);
+        Player p2 = new Player("P2",territory,4,6,1000);
+        p2.move("down");
+        p2.invest(100);
+        assertEquals(p2,territory.region(new int[]{5,6}).getOwner()); //p2 invest 5,6 region
+        p1.shoot("downright",1000);//p1 shoot 5,6
+        assertNull(territory.region(new int[]{5,6}).getOwner());
+    }
+
+    @Test
+    void ShootOpponentCenterCity(){
+        Territory territory = new Territory();
+        Player p1 = new Player("P1",territory,6,6,10000);
+        Player p2 = new Player("P2",territory,7,7,1000);
+        assertFalse(p2.lose());
+        p1.shoot("downright",1000);//p1 shoot 7,7
+        assertTrue(p2.lose());
+
     }
 }
