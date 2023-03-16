920f1b99949e9a0badc1d0a352dcff1e902c7d36
Pichayoot <pichayoothunchainao@gmail.com>
2023-03-16 09:39:39 +0700

    etc

--- front-end/game/src/pages/Game.jsx
+++ front-end/game/src/pages/Game.jsx
@@ -2,20 +2,22 @@ import BaseLayout from "@/components/BaseLayout";
 import Hexgrid from "@/components/Hex";
 import InputPlan from "../components/Input";
 import Timer from "../components/Timer";
+import Frame from "../components/Frame";
 
 export default function Home() {
   return (
     <div className="backofgame">
       <BaseLayout>
+        <Frame />
         <Timer />
-
-        <Hexgrid />
+        {/* <Hexgrid /> */}
         <InputPlan />
       </BaseLayout>
 
       <style jsx>{`
         .backofgame {
-          background-image: url("/images/flower.png");
+          background-image: url("/images/test wallpaper.png");
+          // background-image: url("/images/flower.png");
           background-size: cover;
           background-position: center;
         }
