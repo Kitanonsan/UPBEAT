7a1d7a5f4977739e1419c504e6fb0b0849fc71b2
Kitanonsan <Kitanon_san@cmu.ac.th>
2023-04-03 02:33:29 +0700

    Make Territory in Class GameMessage send only Regions that have owner

--- src/main/java/UPBEAT/WebSocket/GameMessage.java
+++ src/main/java/UPBEAT/WebSocket/GameMessage.java
@@ -9,17 +9,25 @@ import java.util.ArrayList;
 public class GameMessage {
     private PlayerMessage[] Player;
     private RegionMessage[] Territory;
+    private int Row;
+    private int Column;
     GameMessage(Player player1, Player player2,Territory territory){
         this.Player = new PlayerMessage[2];
         this.Territory = new RegionMessage[territory.getM()+ territory.getN()];
         Player[0] = new PlayerMessage(player1);
         Player[1] = new PlayerMessage(player2);
         ArrayList<RegionMessage> arrayList = new ArrayList<>();
+        int count = 0;
         for(int i = 0 ; i < territory.getM() ; i++){
             for(int j = 0 ; j < territory.getN() ; j++){
-                arrayList.add(new RegionMessage(territory.region(new int[]{i,j})));
+                if(territory.region(new int[]{i,j}).getOwner() != null){
+                    arrayList.add(new RegionMessage(territory.region(new int[]{i,j})));
+                    count++;
+                }
             }
         }
-        Territory = arrayList.toArray(Territory);
+        Territory = arrayList.toArray(new RegionMessage[count]);
+        Row = territory.row();
+        Column = territory.column();
     }
 }
