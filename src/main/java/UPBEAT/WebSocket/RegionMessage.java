18a2383f7341500b6b971109b82a626fca6d7da4
Kitanonsan <Kitanon_san@cmu.ac.th>
2023-04-03 00:22:48 +0700

    Delete interestRate and Position in RegionMessage

--- src/main/java/UPBEAT/WebSocket/RegionMessage.java
+++ src/main/java/UPBEAT/WebSocket/RegionMessage.java
@@ -4,16 +4,12 @@ import game.Region;
 import lombok.Getter;
 @Getter
 public class RegionMessage {
-    private int[] position;
     private int deposit;
-    private double interestRate;
     private String owner;
     private boolean isCenterCity;
 
     RegionMessage(Region r){
-        position = r.getPosition();;
         deposit = r.getDeposit();
-        interestRate = (double) r.getInterestRate();
         if(r.getOwner() == null)
             owner = "";
         else
