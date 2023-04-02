d71ce5ec9150330a36c4915668d25ab1541a99ee
Pichayoot <pichayoothunchainao@gmail.com>
2023-04-03 05:59:21 +0700

    Add sendConfiguration in GameController

--- src/main/java/UPBEAT/WebSocket/ConfigurationMessage.java
+++ src/main/java/UPBEAT/WebSocket/ConfigurationMessage.java
@@ -1,18 +1,27 @@
 package UPBEAT.WebSocket;
+import game.Configuration;
 import lombok.Getter;
 
 @Getter
 public class ConfigurationMessage {
     private int m;
     private int n;
-    private int init_plan_min;
-    private int init_plan_sec;
+    private final int init_plan_min;
+    private final int init_plan_sec;
     private int init_budget;
     private int init_center_dep;
-    private int plan_rev_min;
-    private int plan_rev_sec;
+    private final int plan_rev_min;
+    private final int plan_rev_sec;
     private int rev_cost;
     private int max_dep;
     private int interest_pct;
     private int start_deposit;
+
+    public ConfigurationMessage(){
+        Configuration config = Configuration.instance();
+        this.init_plan_min = config.getInit_plan_min();
+        this.init_plan_sec = config.getPlan_rev_sec();
+        this.plan_rev_min = config.getPlan_rev_min();
+        this.plan_rev_sec = config.getPlan_rev_sec();
+    }
 }
